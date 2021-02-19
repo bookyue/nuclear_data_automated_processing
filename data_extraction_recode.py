b7dcec161f8eb87dad0a2569b50ef3e4447d8d74
bookyue <sevenbookyue@gmail.com>
2021-02-19 21:09:31 +0800

    Complete class InputXmlFile. It's awesome!!!
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- data_extraction_recode.py
+++ data_extraction_recode.py
@@ -1,48 +1,19 @@
-import codecs
-from decimal import Decimal
-
-import pandas as pd
-# from cyberbrain import trace
-
 from utils import configlib
+from utils.input_xml_file import InputXmlFile
 
 
-def row_numbers_of_physical_quantity(file_name, physical_quantity_name):
-    list_of_strings_to_search = configlib.Config.get_data_extraction_conf("keys_of_rows").get(physical_quantity_name)
-    physical_quantity_list = configlib.physical_quantity_list_generator(physical_quantity_name)
-
-    index_start = list_of_strings_to_search[:-1]
-    index_end = list_of_strings_to_search[-1]
-
-    length_of_physical_quantity = {'isotope': [],
-                                   'radioactivity': [],
-                                   'absorption': [],
-                                   'fission': [],
-                                   'decay_heat': [],
-                                   'gamma_spectra': []
-                                   }
-
-    i = -1
-    is_find_start_title = False
-    with codecs.open(file_name, 'r', encoding='utf-8') as file:
-        for row_number, line in enumerate(file):
-            if not is_find_start_title:
-                for string_to_search in index_start:
-                    if string_to_search in line:
-                        i += 1
-                        is_find_start_title = True
-                        length_of_physical_quantity[physical_quantity_list[i]].append(
-                            row_number + 7 if physical_quantity_list[i] != 'gamma_spectra' else row_number + 2)
-
-            if is_find_start_title:
-                if index_end in line:
-                    is_find_start_title = False
-                    length_of_physical_quantity[physical_quantity_list[i]].append(
-                        row_number - 3 if physical_quantity_list[i] != 'gamma_spectra' else row_number - 2)
-                    if physical_quantity_name != 'all':
-                        break
-    print(file_name)
-    print(length_of_physical_quantity)
+def extract_rows(xml_file):
+    # text = []
+    # for key in physical_quantity_list:
+    #     row_start = row_numbers[key][0]
+    #     row_end = row_numbers[key][1]
+    #     text.append(file_lines[row_start:row_end + 1])
+    # test = {key: value for key, value in zip(physical_quantity_list, text)}
+    # print(test)
+    # print(file_name)
+    text = []
+    for key in xml_file.chosen_physical_quantity:
+        pass
 
 
 def process(file_path, physical_quantity_name):
@@ -51,14 +22,19 @@ def process(file_path, physical_quantity_name):
 
     file_names = file_path.glob("*.out")
     for file_name in file_names:
-        row_numbers_of_physical_quantity(file_name, physical_quantity_name)
+        xml_file = InputXmlFile(file_name, physical_quantity_name)
+        print(xml_file.name)
+        print(xml_file.chosen_physical_quantity)
+        print(xml_file.unfetched_physical_quantity)
+        print(xml_file.length_of_physical_quantity)
+        extract_rows(xml_file)
 
 
 def main():
     fission_light_nuclide_list = configlib.Config.get_nuclide_list("fission_light")
     test_file_path = configlib.Config.get_file_path("test_file_path")
     step_numbers = configlib.Config.get_data_extraction_conf("step_numbers")
-    physical_quantity_name = "all"
+    physical_quantity_name = "gamma_spectra"
     is_all_step = False
 
     process(file_path=test_file_path, physical_quantity_name=physical_quantity_name)
--- utils/configlib.py
+++ utils/configlib.py
@@ -45,9 +45,3 @@ class Config:
     def get_short_lives_nuclide_list(cls):
         return cls.conf.get("nuclide_list").get("short_lives")
 
-
-def physical_quantity_list_generator(physical_quantity_name):
-    all_physical_quantity_list = ['isotope', 'radioactivity', 'absorption', 'fission', 'decay_heat', 'gamma_spectra']
-    if physical_quantity_name not in all_physical_quantity_list and physical_quantity_name !='all':
-        physical_quantity_name = 'all'
-    return all_physical_quantity_list if physical_quantity_name == 'all' else [physical_quantity_name]
