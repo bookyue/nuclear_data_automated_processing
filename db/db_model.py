3515214893f990c2b58682e5216193c1820938ab
bookyue <sevenbookyue@gmail.com>
2021-02-26 20:55:56 +0800

    Correct ERD
                                        +-------------------+             +----------------+
                                    many|                   |many      one|                |
                  +---------------------+      nuc_data     +------------>+       nuc      |
                  |                     |                   |             |                |
                  |                     +-------------------+             +----------------+
                  |                                  |many
                  |                                  |
                  |                                  |
                  |                                  |
                  |many                              |
    +--------------------------+                     |
    |                          |                     |
    |    physical_quantities   |                     |
    |                          |                     |
    |                          |                     |
    +--------------------------+                     |
                  |many                              |
                  |                                  |
                  |                                  |
                  |                                  |
                  |                                  |
                  |             +----------------+   |
                  |         many|                |   |
                  +-------------+     files      +<--+
                                |                | one
                                +----------------+
    
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- db/db_model.py
+++ db/db_model.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import relationship
 
 from db.base import Base
 
-files_nuc_data_association = Table('files_nuc_data_association', Base.metadata,
-                                   Column('file_id', Integer, ForeignKey('files.id')),
-                                   Column('nuc_data_id', Integer, ForeignKey('nuc_data.id'))
-                                   )
+# files_nuc_data_association = Table('files_nuc_data_association', Base.metadata,
+#                                    Column('file_id', Integer, ForeignKey('files.id')),
+#                                    Column('nuc_data_id', Integer, ForeignKey('nuc_data.id'))
+#                                    )
 
 files_physical_quantities_association = Table('files_physical_quantities_association', Base.metadata,
                                               Column('file_id', Integer, ForeignKey('files.id')),
@@ -32,10 +32,11 @@ class NucData(Base):
     __tablename__ = 'nuc_data'
     id = Column(Integer, primary_key=True, autoincrement=True)
     nuc_id = Column(Integer, ForeignKey('nuc.id'))
+    file_id = Column(Integer, ForeignKey('files.id'))
     data1 = Column(Numeric)
     data2 = Column(Numeric)
 
-    files = relationship('File', secondary=files_nuc_data_association)
+    # files = relationship('File', secondary=files_nuc_data_association)
     physical_quantities = relationship('PhysicalQuantity', secondary=nuc_data_physical_quantities_association)
 
 
@@ -43,8 +44,9 @@ class File(Base):
     __tablename__ = 'files'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
+    data = relationship("NucData", backref='files')
 
-    data = relationship('NucData', secondary=files_nuc_data_association)
+    # data = relationship('NucData', secondary=files_nuc_data_association)
     physical_quantities = relationship('PhysicalQuantity', secondary=files_physical_quantities_association)
 
 
