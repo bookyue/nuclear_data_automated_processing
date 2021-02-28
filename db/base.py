7f1203e8c7425061f2a6d95f9932fd6f94680669
bookyue <sevenbookyue@gmail.com>
2021-02-28 11:08:18 +0800

    add mysql and postgresql connector
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- db/base.py
+++ db/base.py
@@ -1,12 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from utils import configlib
 
 # create an engine
 sqlite_filepath = configlib.Config.get_database_config('path')
-engine = create_engine(f'sqlite:///{sqlite_filepath}')
+# SQLite
+# engine = create_engine(f'sqlite:///{sqlite_filepath}', echo=True)
+# mysql
+engine = create_engine('mysql+mysqlconnector://user:password@localhost:3306/db?charset=utf8mb4', echo=True)
+# postgres
+# engine = create_engine('postgresql+psycopg2://user:password@localhost:5432/db?client_encoding=utf8',
+#                        executemany_mode='batch')
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
