6b8165ae1aeadee5d7faa917d216eb52a078a9e8
bookyue <sevenbookyue@gmail.com>
2021-03-04 16:00:58 +0800

    use mysql upsert method
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- fill_db.py
+++ fill_db.py
@@ -1,8 +1,10 @@
 from decimal import Decimal
+import pandas as pd
 
-from sqlalchemy.orm import scoped_session
+import pangres
+from sqlalchemy.dialects.mysql import insert
 
-from db.base import Base, engine, session_factory
+from db.base import Base, engine, Session
 from db.db_model import Nuc, NucData, File, PhysicalQuantity
 from utils import configlib
 from utils.input_xml_file import InputXmlFileReader
@@ -13,51 +15,67 @@ def init_db():
     Base.metadata.create_all(engine)
 
 
-def populate_database(session, xml_file):
-    session_thread_local = session()
+def _upsert(model, data, update_field):
+    """
+    on_duplicate_key_update for mysql
+    """
+    stmt = insert(model).values(data)
+    d = {f: getattr(stmt.inserted, f) for f in update_field}
+    return stmt.on_duplicate_key_update(**d)
+
+
+def populate_database(xml_file):
+    session = Session()
 
     # file
-    file_tmp = (session_thread_local.query(File)
+    file_tmp = (session.query(File)
                 .filter(File.name == xml_file.name)
                 .one_or_none()
                 )
     if file_tmp is None:
         file_tmp = File(name=xml_file.name)
-        session_thread_local.add(file_tmp)
+        session.add(file_tmp)
 
     for key in xml_file.table_of_physical_quantity:
 
         nuc_data = []
 
         # physical_quantity
-        physical_quantity_tmp = (session_thread_local.query(PhysicalQuantity)
+        physical_quantity_tmp = (session.query(PhysicalQuantity)
                                  .filter(PhysicalQuantity.name == key)
                                  .one_or_none()
                                  )
         if physical_quantity_tmp is None:
             physical_quantity_tmp = PhysicalQuantity(name=key)
-            session_thread_local.add(physical_quantity_tmp)
+            session.add(physical_quantity_tmp)
 
         file_tmp.physical_quantities.append(physical_quantity_tmp)
         physical_quantity_tmp.files.append(file_tmp)
+
+        df_all_tmp = pd.DataFrame(data.split() for data in xml_file.table_of_physical_quantity[key])
+
+        if key == 'gamma_spectra':
+            df_nuc_tmp: pd.DataFrame = df_all_tmp.iloc[:, [0]]
+            df_nuc_tmp.insert(0, 'nuc_ix', range(len(df_nuc_tmp)))
+        else:
+            df_nuc_tmp: pd.DataFrame = df_all_tmp.iloc[:, [0, 1]]
+
+        df_nuc_tmp.columns = ('nuc_ix', 'name')
+        stmt = _upsert(Nuc, df_nuc_tmp.to_dict(orient='records'), update_field=df_nuc_tmp.columns.values.tolist())
+        session.execute(stmt)
 
         for line in xml_file.table_of_physical_quantity[key]:
             elements = line.split()
 
             # nuc
             if key == 'gamma_spectra':
-                nuc_id = None
                 nuc_name = elements[0]
             else:
-                nuc_id = elements[0]
                 nuc_name = elements[1]
-            nuc_tmp = (session_thread_local.query(Nuc)
+            nuc_tmp = (session.query(Nuc)
                        .filter(Nuc.name == nuc_name)
                        .one_or_none()
                        )
-            if nuc_tmp is None:
-                nuc_tmp = Nuc(nuc_ix=nuc_id, name=nuc_name)
-                session_thread_local.add(nuc_tmp)
 
             # nuc_data
             data_tmp = NucData(data1=Decimal(elements[-2]), data2=Decimal(elements[-1]))
@@ -68,20 +86,23 @@ def populate_database(session, xml_file):
 
             nuc_data.append(data_tmp)
 
-        session_thread_local.add_all(nuc_data)
-        session_thread_local.commit()
-    session_thread_local.close()
+        session.add_all(nuc_data)
+        session.commit()
+    session.close()
 
 
 def main():
     test_file_path = configlib.Config.get_file_path('test_file_path')
     physical_quantity_name = 'all'
     init_db()
-    session = scoped_session(session_factory)
+
+    # sqlite:      928.97s user 0.57s system 99% cpu 15:32.94 total
+    # mysql:      1456.85s user 104.03s system 52% cpu 49:55.42 total
+    # postgresql: 1010.68s user 31.02s system 67% cpu 25:52.97 total
     file_names = test_file_path.glob('*.out')
     for file_name in file_names:
         with InputXmlFileReader(file_name, physical_quantity_name) as xml_file:
-            populate_database(session, xml_file)
+            populate_database(xml_file)
 
 
 if __name__ == '__main__':
