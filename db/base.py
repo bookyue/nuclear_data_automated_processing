c5966155c2c2e3082512ec57d31cb614af827090
bookyue <sevenbookyue@gmail.com>
2021-03-07 16:38:25 +0800

    remove sqlite support
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- db/base.py
+++ db/base.py
@@ -9,19 +9,14 @@ def _choose_db(db_type, debug=False):
     db_config = configlib.Config.get_database_config()
 
     if db_type not in db_config:
-        db_type = 'sqlite'
+        db_type = 'mysql'
     user = db_config[db_type].get('user')
     password = db_config[db_type].get('password')
     url = db_config[db_type].get('url')
     port = db_config[db_type].get('port')
     db_name = db_config[db_type].get('dbname')
-    # sqlite
-    path = db_config[db_type].get('path')
 
-    if db_type == 'sqlite':
-        connector_string = f'sqlite:///{path}'
-        engine_tmp = create_engine(connector_string, future=True, echo=debug)
-    elif db_type == 'mysql':
+    if db_type == 'mysql':
         connector_string = f'mysql+mysqlconnector://{user}:{password}@{url}:{port}/{db_name}?charset=utf8mb4'
         engine_tmp = create_engine(connector_string, future=True, echo=debug)
     else:
@@ -37,4 +32,4 @@ def _choose_db(db_type, debug=False):
 
 
 Base = declarative_base()
-engine, Session = _choose_db('mysql', debug=False)
+engine, Session = _choose_db('postgresql', debug=False)
