b248cf80c84f22ab8b264eddb03d54a83cb89f96
bookyue <sevenbookyue@gmail.com>
2021-03-08 13:32:15 +0800

    add partial input for physical_quantity
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- utils/physical_quantity_list_generator.py
+++ utils/physical_quantity_list_generator.py
@@ -1,5 +1,20 @@
+from utils.configlib import Config
+
+
+def _get_physical_quantity_list_by_conf_file():
+    return [key for key in Config.get_data_extraction_conf('keys_of_rows') if key != 'all']
+
+
+all_physical_quantity_list = _get_physical_quantity_list_by_conf_file()
+
+
 def physical_quantity_list_generator(physical_quantity_name):
-    all_physical_quantity_list = ['isotope', 'radioactivity', 'absorption', 'fission', 'decay_heat', 'gamma_spectra']
-    if physical_quantity_name not in all_physical_quantity_list and physical_quantity_name !='all':
+    if isinstance(physical_quantity_name, str):
+        physical_quantity_name = [physical_quantity_name]
+
+    if not all(single in all_physical_quantity_list for single in set(physical_quantity_name)) \
+            and physical_quantity_name != 'all':
         physical_quantity_name = 'all'
-    return all_physical_quantity_list if physical_quantity_name == 'all' else [physical_quantity_name]
+
+    return all_physical_quantity_list if physical_quantity_name == 'all' else list(set(physical_quantity_name))
+
