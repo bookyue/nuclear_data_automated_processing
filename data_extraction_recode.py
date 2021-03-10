b8e955ec803d59d361d6879bcf003d1fbfd26e05
bookyue <sevenbookyue@gmail.com>
2021-03-10 09:27:24 +0800

    filter_data is not required any more.
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- data_extraction_recode.py
+++ data_extraction_recode.py
@@ -1,42 +1,42 @@
-import pandas as pd
-
+from db.fetch_data import fetch_all_filenames, fetch_data_by_filename_and_nuclide_list
 from utils.configlib import Config
-from db.fetch_data import fetch_all_filenames, fetch_data_by_filename
 
 
-def filter_data(dict_df_data, nuclide_list):
+def filter_data(filename, physical_quantity_name, nuclide_list):
     """
     依据核素名称，筛选数据行
-    :param dict_df_data: dictionary
+    :param filename: File object
+    :param physical_quantity_name: str or list or PhysicalQuantity object
     :param nuclide_list: list, 关键核素，如果为None，则取不为0的全部核素
     :return: DataFrame，关键核素的计算结果
     """
-    if (nuclide_list is not None) and (len(nuclide_list) > 1):
-        for key in dict_df_data:
-            if key == 'gamma_spectra':
-                continue
-            is_in_nuclide_list = dict_df_data[key]['nuc_name'].isin(nuclide_list)
-            dict_df_data[key]: pd.DataFrame = dict_df_data[key].loc[is_in_nuclide_list]
-    else:
-        # Drop rows with all zeros in data.
-        for key in dict_df_data:
-            """
-            The two lines below, does the exact same thing here.
-            Drop rows with all zeros. But the latter one is way much faster.
-            df_filter = df_data.iloc[df_data.apply(np.sum, axis=1).to_numpy().nonzero()]
-            df_filter = df_data.iloc[df_data.any(axis=1).to_numpy().nonzero()]
-
-            When only condition is provided,
-            the numpy.where() function is a shorthand for np.asarray(condition).nonzero().
-            Using nonzero directly should be preferred, as it behaves correctly for subclasses.
-            The rest of this documentation covers only the case where all three arguments are provided.
-            But to be clear, I leave the np.where() kind of function in this comment.
-            df_is_not_zero = np.where(df_density.any(axis=1))
-            """
-
-            df_is_not_zero = dict_df_data[key][['first_step', 'last_step']].any(axis=1).to_numpy().nonzero()
-            dict_df_data[key] = dict_df_data[key].iloc[df_is_not_zero]
-            # df_output = df_data.loc[(df_density != Decimal(0)).any(axis=1), :]
+    # if (nuclide_list is not None) and (len(nuclide_list) > 1):
+    #     for key in dict_df_data:
+    #         if key == 'gamma_spectra':
+    #             continue
+    #         is_in_nuclide_list = dict_df_data[key]['nuc_name'].isin(nuclide_list)
+    #         dict_df_data[key]: pd.DataFrame = dict_df_data[key].loc[is_in_nuclide_list]
+    # else:
+    #     # Drop rows with all zeros in data.
+    #     for key in dict_df_data:
+    #         """
+    #         The two lines below, does the exact same thing here.
+    #         Drop rows with all zeros. But the latter one is way much faster.
+    #         df_filter = df_data.iloc[df_data.apply(np.sum, axis=1).to_numpy().nonzero()]
+    #         df_filter = df_data.iloc[df_data.any(axis=1).to_numpy().nonzero()]
+    #
+    #         When only condition is provided,
+    #         the numpy.where() function is a shorthand for np.asarray(condition).nonzero().
+    #         Using nonzero directly should be preferred, as it behaves correctly for subclasses.
+    #         The rest of this documentation covers only the case where all three arguments are provided.
+    #         But to be clear, I leave the np.where() kind of function in this comment.
+    #         df_is_not_zero = np.where(df_density.any(axis=1))
+    #         """
+    #
+    #         df_is_not_zero = dict_df_data[key][['first_step', 'last_step']].any(axis=1).to_numpy().nonzero()
+    #         dict_df_data[key] = dict_df_data[key].iloc[df_is_not_zero]
+    #         # df_output = df_data.loc[(df_density != Decimal(0)).any(axis=1), :]
+    dict_df_data = fetch_data_by_filename_and_nuclide_list(filename, physical_quantity_name, nuclide_list)
 
     return dict_df_data
 
@@ -95,8 +95,7 @@ def process(physical_quantity_name, nuclide_list):
     filenames = fetch_all_filenames()
     for filename in filenames:
         # print(filename.name)
-        dict_df_data = fetch_data_by_filename(filename, physical_quantity_name)
-        df_nuclide_list = filter_data(dict_df_data, nuclide_list)
+        df_nuclide_list = filter_data(filename, physical_quantity_name, nuclide_list)
         # print(df_nuclide_list)
 
 
