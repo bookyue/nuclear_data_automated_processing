f0445ee30ab2284af882d4736fbf04ed7c4a98f8
bookyue <sevenbookyue@gmail.com>
2021-03-14 14:41:40 +0800

    add doc for middle_steps.py
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- utils/middle_steps.py
+++ utils/middle_steps.py
@@ -17,14 +17,36 @@ def parsing(middle_steps_str):
     return (middle_step for middle_step in middle_steps.middle_steps)
 
 
-def middle_steps_line_serialization(data):
+def middle_steps_line_serialization(data: list):
+    """
+    将middle_steps_line序列化
+    len(data)小于10的话说明没有中间步骤，直接返回
+    Parameters
+    ----------
+    data : list
+
+    Returns
+    -------
+    list
+    """
     if len(data) < 10:
         return data
     middle_steps = serialization(data[3: -1])
     return [*data[0:3], data[-1], middle_steps]
 
 
-def middle_steps_line_parsing(data):
+def middle_steps_line_parsing(data: list):
+    """
+    将middle_steps_line反序列化，并返回一个含序号和数据的字典
+
+    Parameters
+    ----------
+    data : list
+
+    Returns
+    -------
+    dict
+    """
     if data is None:
         return {'middle_steps': None}
     else:
