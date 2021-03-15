d7552e217f7870eeead44c7eb5236a37283fb8eb
bookyue <sevenbookyue@gmail.com>
2021-03-15 21:02:12 +0800

    mv init_db to db_utils
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- db/db_utils.py
+++ db/db_utils.py
@@ -3,6 +3,18 @@ from sqlalchemy import delete
 from db.base import Session, Base
 
 
+def init_db():
+    """
+    初始化数据库
+    Returns
+    -------
+
+    """
+    session = Session()
+    Base.metadata.drop_all(session.bind)
+    Base.metadata.create_all(session.bind)
+
+
 def delete_all_from_table(model):
     """
     删除某表的全部records
