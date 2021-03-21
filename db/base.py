ab691649cdf2d0d77b21900f2bf81c3450b2509a
bookyue <sevenbookyue@gmail.com>
2021-03-21 21:01:40 +0800

    restore sqlite in base.py
    Signed-off-by: bookyue <sevenbookyue@gmail.com>

--- db/base.py
+++ db/base.py
@@ -23,14 +23,19 @@ def _choose_db(db_type, debug=False):
     db_config = configlib.Config.get_database_config()
 
     if db_type not in db_config:
-        db_type = 'mysql'
+        db_type = 'sqlite'
     user = db_config[db_type].get('user')
     password = db_config[db_type].get('password')
     url = db_config[db_type].get('url')
     port = db_config[db_type].get('port')
     db_name = db_config[db_type].get('dbname')
+    # sqlite
+    path = db_config[db_type].get('path')
 
-    if db_type == 'mysql':
+    if db_type == 'sqlite':
+        connector_string = f'sqlite:///{path}'
+        engine_tmp = create_engine(connector_string, future=True, echo=debug)
+    elif db_type == 'mysql':
         connector_string = f'mysql+mysqlconnector://{user}:{password}@{url}:{port}/{db_name}?charset=utf8mb4'
         engine_tmp = create_engine(connector_string, future=True, echo=debug)
     else:
